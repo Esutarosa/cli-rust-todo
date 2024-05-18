77d028454595be78aa353712b210d3f3981ebd93
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-05-18 21:27:11 +0300

    + Todo list implementation

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct TodoItem {
-    id: String,
+    id: usize,
     description: String,
     completed: bool,
 }
@@ -13,6 +13,56 @@ struct TodoItem {
 #[derive(Serialize, Deserialize, Debug)]
 struct TodoList {
     items: Vec<TodoItem>,
+}
+
+impl TodoList {
+    fn new() -> Self {
+        Self { items: Vec::new() }
+    }
+
+    fn add(&mut self, description: String) {
+        let id: usize = self.items.len() + 1;
+        self.items.push(TodoItem {
+            id,
+            description,
+            completed: false,
+        });
+    }
+
+    fn complete(&mut self, id: usize) {
+        if let Some(item) = self.items.iter_mut().find(|i| i.id == id) {
+            item.completed = true;
+        }
+    }
+
+    fn list(&self) {
+        for item in &self.items {
+            println!(
+                "{}: {} [{}]",
+                item.id,
+                item.description,
+                if item.completed { "x" } else { " " }
+            );
+        }
+    }
+
+    fn save(&self, filename: &str) -> io::Result<()> {
+        let file = File::create(filename)?;
+        let writer = BufWriter::new(file);
+        serde_json::to_writer(writer, &self)?;
+        Ok(())
+    }
+
+    fn load(filename: &str) -> io::Result<Self> {
+        if Path::new(filename).exists() {
+            let file = File::open(filename)?;
+            let reader = BufReader::new(file);
+            let list = serde_json::from_reader(reader)?;
+            Ok(list)
+        } else {
+            Ok(TodoList::new())
+        }
+    }
 }
 
 fn main() {}
